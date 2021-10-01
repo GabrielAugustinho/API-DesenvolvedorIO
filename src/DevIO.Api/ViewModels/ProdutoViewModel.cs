e7466a0ed85c8e526e66604a235b0f6c2ef43b65
GabrielAugustinho <gabriel.aug.alves@outlook.com>
2021-10-01 16:26:35 -0300

    Adicionando ViewsModels para evitar erros de conexão explícita com o entity

---
+++ src/DevIO.Api/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.Api.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entr {2} e {1} caracteres"), MinLength(2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entr {2} e {1} caracteres"), MinLength(2)]
+        public string Descricao { get; set; }
+
+        // Seta o nome da imagem
+        public string Imagem { get; set; }
+        // Dado da imagem
+        public string ImagemUpload { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public decimal Valor { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string NomeFornecedor { get; set; }
+    }
+}
