23b1bc59620bc94562a9afc8802e36f5b42f8cc1
GabrielAugustinho <gabriel.aug.alves@outlook.com>
2021-10-01 16:53:52 -0300

    Configurando um MainController na API e instalando o AutoMapper

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // Validação de notificações de erro
+
+        // Validação de modelstate
+
+        // Validação de operação de negócios
+    }
+
+    [Route("api/[controller]")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+        }
+
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
+        {
+            var fornecedor = await _fornecedorRepository.ObterTodos();
+
+            return Ok(fornecedor);
+        }
+    }
+}
