2e6d7a2c9f064310765207c0ad6f9af2c926a437
GabrielAugustinho <gabriel.aug.alves@outlook.com>
2021-10-06 09:31:32 -0300

    Implementaçõa do identity e controller para segurança

--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -25,6 +25,9 @@ namespace DevIO.Api
             {
                 options.UseSqlServer(Configuration.GetConnectionString(name: "DefaultConnection"));
             });
+
+            services.AddIdentityConfiguration(Configuration);
+
             services.AddAutoMapper(typeof(Startup));
 
             services.WebApiConfig();
@@ -44,6 +47,7 @@ namespace DevIO.Api
                 app.UseHsts();
             }
 
+            app.UseAuthentication();
             app.UseMvcConfiguration();
         }
     }
